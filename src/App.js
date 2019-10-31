a80eb8c4ef3ed44cd0d8a42ea25897dfd3de12a5
mubashir <mubashir_meraj@ymail.com>
2019-11-01 02:31:02 +0500

    router set up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage.component";
-
+const Hats = () => <h1>HATS</h1>;
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/shop/hats" component={Hats} />
+      </Switch>
     </div>
   );
 }
