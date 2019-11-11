684f7563666a8118a2a2c98f3a01134e02909fe0
mubashir <mubashir_meraj@ymail.com>
2019-11-12 00:58:45 +0500

    shop page, collection items, header, sign in form

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
-import HomePage from "./pages/homepage.component";
-const Hats = () => <h1>HATS</h1>;
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import Header from "./components/header/header.component";
+
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route path="/" exact component={HomePage} />
-        <Route path="/shop/hats" component={Hats} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/sign-in" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
